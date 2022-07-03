5b3d15436124d2a681ceb61a05b0b8783afb7cc9
程哥哥 <705328646@qq.com>
2022-07-03 11:14:18 +0800

    test

---
+++ src/com/CGG/tank/Dir.java
@@ -0,0 +1,16 @@
+package com.CGG.tank;
+
+/**
+ * @ClassName Dir  //类名称
+ * @Description: 类描述
+ * @Author: 程哥哥  //作者
+ * @CreateDate: 2022/7/2 21:03	//创建时间
+ * @UpdateUser: 更新人
+ * @UpdateDate: 2022/7/2 21:03	//更新时间
+ * @UpdateRemark: 更新的信息
+ * @Version: 1.0  //版本号
+ */
+
+public enum Dir {
+  LEFT,RIGHT,UP,DOWN
+}
